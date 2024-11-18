efc217731f32d28780406de295f4fba50138e6df
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-11-18 13:46:37 +0100

    first commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div className="[border-bottom:1px_solid_#e2e2e2] p-[1.7rem] flex justify-between items-center bg-[#407bff] text-[white]">
+      <h1 className="text-[25px] font-bold">Bloocode Movie</h1>
+      <div>
+        <h1>Favorite</h1>
+      </div>
+    </div>
+  );
+}
